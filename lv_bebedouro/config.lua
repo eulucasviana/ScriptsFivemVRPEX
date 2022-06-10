39126c2b399ce3d949d0578cf7c8be4a9f28a0bc
Lucas Viana <93997303+eulucasviana@users.noreply.github.com>
2022-06-10 11:53:06 -0300

    [ADD] Script lv_bebedouro

---
+++ lv_bebedouro/config.lua
@@ -0,0 +1,8 @@
+config = {}
+
+config.propbebedouro = {
+    { 'prop_watercooler' }
+}
+
+config.itemrecebido = 'agua'
+config.itempreciso = 'garrafavazia'
---
+++ lv_bebedouro/fxmanifest.lua
@@ -0,0 +1,16 @@
+shared_script "@vrp/lib/lib.lua" 
+
+fx_version 'adamant'
+game 'gta5'
+
+client_scripts {
+	'@vrp/lib/utils.lua',
+	'config.lua',
+	'client/*.lua'
+}
+
+server_scripts {
+	'@vrp/lib/utils.lua',
+	'config.lua',
+	'server/*.lua'
+}      
